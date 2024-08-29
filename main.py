135c03ee1c92c894a626091b3a28749ac8009ba8
Muhammed <muhammedenes173@gmail.com>
2024-08-30 00:06:17 +0300

    Harf tahmin oyunu yapıldı

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-my_list=[10,20,30,40]
+from random import randint
 
-for num  in my_list:
-    print(num)
-print("test")
+can = 3
+
+sayi = randint(1,10)
+while True:
+    tahmin = int(input("Enter the 0-10 numbers :"))
+
+    if sayi == tahmin:
+        print("Doğru!!!")
+        print("Canınız :",can)
+        break
+    else:
+        print("Tekrar Deneyiniz")
+        can = can - 1
+        print(f"Canınız : {can}")
+
+    if can == 0:
+        print("Kaybettiniz...")
+        print("Sayı : ", sayi)
+        break
